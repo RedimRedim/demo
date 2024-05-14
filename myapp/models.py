e99f58d86a53a7421aeeebf4644523379bedbf04
Dwi Saputra W <dwisaputrano10@gmail.com>
2024-05-14 18:49:19 +0700

    add table

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
 
 class todoitem(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+class Sales(models.Model):
+    orderDate = models.DateField()
+    totalOrders = models.IntegerField()
+    completedOrders = models.IntegerField()
+    totalAmount = models.DecimalField(max_digits=10, decimal_places=2)
+    ratio = models.CharField(max_length=10)
+    completedTime = models.TimeField()
+
+    def __str__(self):
+        return f"Sales data for {self.orderDate}"
+    
+    
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render,HttpResponse
 from .models import todoitem
+from myapp.models import Sales
+from datetime import date, time
 # request handler request -> response is being called views.py in Django
 # Create your views here.
 
 
 def home(request):
     return render(request,"home.html")
+
+sales_data = Sales(
+    orderDate=date(2024, 5, 1),
+    totalOrders=3000,
+    completedOrders=2500,
+    totalAmount=1325828,
+    ratio='83%',
+    completedTime=time(hour=0, minute=1, second=15)
+)
+sales_data.save()
 
 
 
